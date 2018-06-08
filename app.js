cd572b593833f9d5ee32df6b8c2d16c7c1197b3c
acesdatar <reign.derek@yahoo.com>
2018-06-08 18:17:30 +0000

    Setup heroku and start script

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const hbs = require('hbs');
 const app = express();
 const fs = require('fs');
-const PORT = 3000;
+const PORT = process.env || 3000;
 
 
 hbs.registerPartials(__dirname + '/views/partials');
